a311103563040b66baa98da83dd89bbd18d8aa73
Ishancorp <sharma.ishan450@gmail.com>
2025-08-26 19:30:20 -0700

    Simplified code part

--- src/infoboxes/homepage/HomePage.jsx
+++ src/infoboxes/homepage/HomePage.jsx
@@ -41,30 +41,11 @@ export default function HomePage() {
               Use Wikipedia-style markup in text fields:
             </p>
             <ul>
-              <li>
-                <code>'''bold'''</code> → {parseTextWithSpans(`'''bold'''`)}
-              </li>
-              <li>
-                <code>''italic''</code> → {parseTextWithSpans(`''italics''`)}
-              </li>
-              <li>
-                <code>*link*</code> → {parseTextWithSpans(`*bold*`)}
-              </li>
-              <li>
-                <code>x^{"{sup}"}</code> → {parseTextWithSpans(`x^{sup}`)}
-              </li>
-              <li>
-                <code>{"{{increase}}"}</code> → {parseTextWithSpans(`{{increase}}`)}
-              </li>
-              <li>
-                <code>{"{{decrease}}"}</code> → {parseTextWithSpans(`{{decrease}}`)}
-              </li>
-              <li>
-                <code>{"--"}</code> → {parseTextWithSpans(`--`)} (em dash)
-              </li>
-              <li>
-                <code>{"{{ color | #ff0000 | red}}"}</code> → {parseTextWithSpans(`{{ color | #ff0000 | red}}`)}
-              </li>
+              {[`'''bold'''`, `''italics''`, `*bold*`, `x^{sup}`, `{{increase}}`, `{{decrease}}`, `--`, "{{ color | #ff0000 | red}}"].map( format =>
+                <li>
+                  <code>{format}</code> → {parseTextWithSpans(format)}
+                </li>
+              )}
             </ul>
           </div>
         </div>
